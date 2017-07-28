212e1348458d7f3990c42966c73769a049d05d41
Jasmine Nurul R <jasmine29797@gmail.com>
2017-07-27 18:43:19 -0700

    tugas3 Java3 Visual

---
+++ src/latihan/MainLat.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan;
+
+/**
+ *
+ * @author VAIO
+ */
+public class MainLat {
+    
+    public static void main(String[] args){
+        MainFont out = new MainFont();
+    
+    out.setVisible(true);
+}
+}
